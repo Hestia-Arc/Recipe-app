514ed5c0d89f2c024c70722fcaea563cf57d4622
Afolabi Esther <starafolab@gmail.com>
2024-02-08 17:34:56 +0100

    add hero section

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,11 @@
 const recipe = 'soups'
+
+async function getRecipes() {
+    const response = await fetch('https://www.themealdb.com/api/json/v2/', {method: "GET"})
+
+    const data = await response.json()
+
+    console.log(data)
+}
+
+getRecipes()
